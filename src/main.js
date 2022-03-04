ab793c590aa76f37cf4098b445368cb773ee0ae2
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2022-03-04 10:53:02 +0500

    change 0.1

--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ import {createTripFiltersTemplate} from './view/trip-filters-view.js';
 import {createTripSortTemplate} from './view/trip-sort-view.js';
 import {createEventEditItemTemplate} from './view/event-edit-item-view.js';
 import {createTripEventsItemTemplate} from './view/trip-events-item-view.js';
-import {createFormCreateTemplate} from './view/event-create-item-view';
+import {createFormCreateTemplate} from './view/event-create-item-view.js';
+import {сreateEventListTemplate} from './view/event-list-view.js';
 
 const TripSiteMenuElement = document.querySelector('.trip-controls__navigation');
 const TripFiltersElement = document.querySelector('.trip-controls__filters');
 const TripEventsElement = document.querySelector('.trip-events');
-const TripEventsListElement = TripEventsElement.querySelector('.trip-events__list');
+
 
 renderTemplate(TripSiteMenuElement, createTripTabsTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(TripFiltersElement, createTripFiltersTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(TripEventsElement, createTripSortTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(TripEventsElement, createTripSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(TripEventsElement, сreateEventListTemplate(), RenderPosition.BEFOREEND);
+
+const TripEventsListElement = TripEventsElement.querySelector('.trip-events__list');
+
 renderTemplate(TripEventsListElement, createFormCreateTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(TripEventsListElement, createEventEditItemTemplate(), RenderPosition.BEFOREEND);
 for (let i = 0; i < 3; i++) {
---
+++ src/view/event-list-view.js
@@ -0,0 +1,4 @@
+export const сreateEventListTemplate = () => (
+   ` <ul class="trip-events__list">
+    </ul>`
+);
