d5d5e54e72ed4cfcc6dea2e94ae47ec4b33315d8
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2022-06-07 15:05:53 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { generateDestPoint } from './mock/destination-event.js';
 import { sortPointsByDate } from './tools/template-tools.js';
 import { reformPoint } from './tools/reformer';
 
+//
 const TRIP_EVENTS_COUNT = 20;
 const tripEvents = sortPointsByDate(Array.from({ length: TRIP_EVENTS_COUNT }, () => reformPoint(generateDestPoint())));
 
